0f928ca51ab34a4b81cbe5c1b01f2ab2c73ad4d8
Fenrir <fenrireclair@gmail.com>
2023-04-23 04:02:05 +0900

    新增老師的專案

---
+++ dicee-flutter-master/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          // flex: 2,
+          // child: Image(
+          //   image: AssetImage('images/dice1.png')
+          // ),
+          child: Image.asset('images/dice1.png'),
+        ),
+        Expanded(
+          // flex: 1,
+          // child: Image(
+          //     image: AssetImage('images/dice1.png')
+          // ),
+          child: Image.asset('images/dice2.png'),
+        ),
+      ],
+    );
+  }
+}
